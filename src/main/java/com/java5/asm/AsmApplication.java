691282d4d0b7d0b5e6a4ad941d821af0e82711f2
ln678090 <ln678090@gmail.com>
2026-04-09 20:53:29 +0700

    commit

--- src/main/java/com/java5/asm/AsmApplication.java
+++ src/main/java/com/java5/asm/AsmApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class AsmApplication {
 
-    static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(AsmApplication.class, args);
     }
 
